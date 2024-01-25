f0b7618c0c7d9d9df98b260552806d0825d4f82f
Syed Osama Ahmed <batch17.026@gmail.com>
2024-01-25 23:17:00 +0500

    Error in bar graph

--- lib/widgets/bar_graph.dart
+++ lib/widgets/bar_graph.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:spech_interview_task/bar%20graph/bar_graph_data.dart';
 
 class BarGraph extends StatelessWidget {
   final List<String> taskStats;
@@ -11,11 +12,27 @@ class BarGraph extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    BarGraphData data = BarGraphData(
+      taskStatsJan: taskStats[0],
+      taskStatsFeb: taskStats[1],
+      taskStatsMarch: taskStats[2],
+      taskStatsApril: taskStats[3],
+    );
+    data.initializeBarData(taskStats);
+    print(taskStats);
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.55,
       child: BarChart(
-        BarChartData(maxY: 100.0, minY: 0.0),
+        BarChartData(
+          maxY: 100.0,
+          minY: 0.0,
+          barGroups: data.barData
+              .map((data) => BarChartGroupData(
+                  x: int.parse(data.months),
+                  barRods: BarChartRodData(toY: double.parse(data.taskStats))))
+              .toList(),
+        ),
         swapAnimationDuration: const Duration(milliseconds: 150),
         swapAnimationCurve: Curves.bounceOut,
       ),
